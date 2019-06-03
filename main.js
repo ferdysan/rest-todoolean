66461c5a7a70862d29ea0077cf44591e666c5e66
Ferdysan <ferdysan87@gmail.com>
2019-06-03 21:21:40 +0200

    inserito data per cancellazione impegno

--- main.js
+++ main.js
@@ -2,40 +2,68 @@
 
 
 $(document).ready(function(){
+
+  var template_html = $('#template_todo').html();
+  var template_function = Handlebars.compile(template_html);
+
+
    var url_base= 'http://157.230.17.132:3009/todos/';
 
+   stampa_impegni();
 
+   $('#bottone_crea').click(function(){
+     var todo_text = $('#new_todo').val();
 
-   $.ajax({
-     'url' :url_base,
-     'method': 'GET',
-     'success': function(data){
-       for (var i = 0; i < data.length; i++) {
-         $('#todos').append('<li>' + data[i].text  + '</li>');
+     $('#new_todo').val('');
+
+     $.ajax({
+       'url' :url_base,
+       'method': 'POST',
+       // gli passo il testo scritto in input dall'utente
+       'data':{
+        'text': todo_text
+       },
+       'success': function(data){
+           stampa_impegni();
+
+       },
+       'error': function(){
+         alert('qualcosa è andato storto')
        }
-     },
-     'error': function(){
-       alert('qualcosa è andato storto')
-     }
+     });
+
+   });
+
+   $('#todos').on('click', 'span', function(){
+     // vado a selezionare tramite l'attributo data impostato nel metodo get l'elemento selezionato
+     var id_deleter = $(this).attr('data-id');
+
+
    });
 
 
 
-   $.ajax({
-     'url' :url_base,
-     'method': 'POST',
-     'data':{
-       'text': todo_text
-     },
-     'success': function(data){
-       
-     },
-     'error': function(){
-       alert('qualcosa è andato storto')
-     }
-   });
 
+  //  creo una funzione da riutilizzare in varie parti del codice
 
-   $('#new_todo_button').click(function())
+   function stampa_impegni(){
+     $('#todos').empty();
+
+     $.ajax({
+       'url' :url_base,
+       'method': 'GET',
+       'success': function(data){
+         for (var i = 0; i < data.length; i++) {
+           $('#todos').append('<li><span data-id="'+data[i].id+'">x</span>' + data[i].text  + '</li>');
+           // var new_todo = data[i].text;
+           // $('#todos').append(template_function(new_todo));
+         }
+       },
+       'error': function(){
+         alert('qualcosa è andato storto')
+       }
+     });
+   }
+
 
 });
